48ecd5ea72863bd84578e493ddab6bff848723ee
shadowhaha <23697394+shadowhaha@users.noreply.github.com>
2023-03-06 09:42:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  words = requests.get("https://api.shadiao.pro/chp", verify=False)
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
