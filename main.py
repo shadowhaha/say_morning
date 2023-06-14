aa7913b5674afcc057fb9f3d58b5c4d7ad762c5e
shadowhaha <23697394+shadowhaha@users.noreply.github.com>
2023-06-14 11:17:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_words():
 
 def get_words2():
   words2 = requests.get("https://saying.api.azwcl.com/saying/get", verify=False)
-  print(words2)
+  print('打印---------------'+words2)
   if words2.json()['code'] != 200:
     return get_words2()
   return words2.json()['data']['content'],words2.json()['data']['author']
